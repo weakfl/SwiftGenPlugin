140d4fc314817de0d96980089a9530e8fd8630bf
weak <weak@fraglab.at>
2024-03-05 13:08:54 +0100

    update v6.6.3

--- Package.swift
+++ Package.swift
@@ -29,7 +29,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "swiftgen",
-            path: "./swiftgen.artifactbundle"
+            url: "https://github.com/SwiftGen/SwiftGen/releases/download/6.6.3/swiftgen-6.6.3.artifactbundle.zip",
+            checksum: "caf1feaf93dd32bc5037f0b6ded8d0f4fe28ab5d2f6e5c3edf2572006ba0b7eb"
         )
     ]
 )
